8bc6bb97bb4afc4c4de057488ff9200c7dbde204
sacm <sacm1046@gmail.com>
2025-02-21 15:43:32 -0300

    service api added

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const baseUrl = process.env.BASE_URL || '/';
-const user = process.env.USER;
-const pass = process.env.PASS;
+const baseUrl = process.env.URL_BASE;
 
 // Middleware para parsear JSON
 app.use(express.json());
 
 // Ruta de prueba
-app.get('/', async (req, res) => {
-    const url = baseUrl + '?user='+user+'@ejemplo.com&pass='+pass+'&function=GetSeries%C3%97eries=F022.TPM.TIN.D001.NO.Z.D&firstdate=2025-02-21&lastdate=2025-02-21'
+app.get('/:currencyType', async (req, res) => {
+    const url = baseUrl + "/" + req.params.currencyType;
+
     console.log(url)
-
     const getSeries = await fetch(url);
-    const series = await getSeries.json();
-
-
+    const series = await getSeries.json(); 
     res.json({ message: 'Hola Mundo', series });
 });
 
