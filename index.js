53c5ee49f66ef19e9431302d98cdf65b86ec2321
sacm <sacm1046@gmail.com>
2025-02-24 20:17:12 -0300

    new service added

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const baseUrl = process.env.URL_BASE;
 
 app.use(express.json());
+
+const formatDate = () => {
+    const today = new Date();
+    const day = today.getDate() < 10 ? `0${today.getDate()}` : today.getDate();
+    const month = today.getMonth() + 1 < 10 ? `0${today.getMonth() + 1}` : today.getMonth() + 1;
+    return `${day}-${month}-${today.getFullYear()}`;
+}
 
 app.get('/random', async (req, res) => {
     try {
@@ -13,6 +21,19 @@ app.get('/random', async (req, res) => {
         console.log(error);
     }
 });
+
+app.get('/:currencyType', async (req, res) => {
+    try {
+        const formattedDate = formatDate();
+        const url = baseUrl + "/" + req.params.currencyType + "/" + formattedDate;
+        const getSeries = await fetch(url);
+        const data = await getSeries.json();
+        res.json(data.serie[0].valor);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
 
 // Iniciar el servidor
 app.listen(port, () => {
